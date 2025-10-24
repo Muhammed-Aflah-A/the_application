10935eb69d23caf1a759b1754689772d4cbaceba
Muhammed Aflah <aflahathimannil@gmail.com>
2025-10-24 22:30:54 +0530

    set shared preference for saving theme

--- lib/notifier/theme_provider.dart
+++ lib/notifier/theme_provider.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:the_app/service/theme_preference.dart';
 
 class ThemeProvider with ChangeNotifier {
+  var theme = ToggleTheme();
   bool _isDark = false;
   bool get isDark => _isDark;
+  ThemeProvider() {
+    loadThemeFromSharedPrefs();
+  }
   void toggleTheme() {
     _isDark = !_isDark;
+    theme.saveTheme(_isDark);
+    notifyListeners();
+  }
+
+  void loadThemeFromSharedPrefs() async {
+    _isDark = await theme.loadTheme();
     notifyListeners();
   }
 }
